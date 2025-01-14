0c750d51a677eb756fae7eebf95b4775073d4ea3
jason <zou.jason@qq.com>
2025-01-14 10:40:40 +0800

    开源发布：统一调整开源协议

--- base_core/__init__.py
+++ base_core/__init__.py
@@ -1,4 +1,6 @@
-# Copyright 2024 中裕软管科技股份有限公司 (http://www.zyfire.cc/)
+# -*- coding: utf-8 -*-
+# Copyright 2025 OpenCoze开源扣子
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 
 from . import models
