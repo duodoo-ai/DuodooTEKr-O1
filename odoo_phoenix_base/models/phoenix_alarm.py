93be9faddc23b3ee153427c4154639b86cfe339f
zou.jason <zou.jason@qq.com>
2025-02-23 19:33:37 +0800

    设备运行监测模块告警数据调整为异常告警池，归集所有的异常信息

--- odoo_phoenix_base/models/phoenix_alarm.py
+++ odoo_phoenix_base/models/phoenix_alarm.py
@@ -12,17 +12,19 @@ _logger = logging.getLogger(__name__)
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
 
-class PhoenixOpticAlarm(models.Model):
-    _name = 'phoenix.optic.alarm'
-    _description = '告警数据'
+class PhoenixAlarmPool(models.Model):
+    _name = 'phoenix.alarm.pool'
+    _description = '告警记录池'
     _order = 'id desc'
 
-    name = fields.Char(string='测点', required=True, index=True, help='从上位系统写入')
-    monitor_time = fields.Datetime(string='日期/时间', help='从上位系统写入。监测时间')
-    monitor_value = fields.Char(string="告警值", help='从上位系统写入')
-    monitor_code = fields.Char(string="序列号", help='从上位系统写入')
-    async_state = fields.Integer(string='告警状态', default=0, help='0：从上位系统写入获得告警状态；1：告警动作已触发')
-    async_time = fields.Datetime(string='告警时间', help='从上位系统写入')
+    name = fields.Char(string='测点', required=True, index=True, help='从下位系统写入')
+    monitor_time = fields.Datetime(string='日期/时间', help='从下位系统写入。监测时间')
+    monitor_value = fields.Char(string="告警值", help='从下位系统写入')
+    monitor_code = fields.Char(string="序列号", help='从下位系统写入')
+    async_state = fields.Integer(string='告警状态', default=0, help='0：从下位系统写入获得告警状态；1：告警动作已触发')
+    async_time = fields.Datetime(string='告警时间', help='从下位系统写入')
+    active = fields.Boolean(string='状态', defautl=True, help='从下位系统写入')
+    default_code = fields.Char(string='序号', index=True, help='从下位系统写入')
     company_id = fields.Many2one(
         'res.company',
         string='公司',
@@ -32,6 +34,6 @@ class PhoenixOpticAlarm(models.Model):
     def create(self, vals_list):
         for val in vals_list:
             if not val.get('default_code'):
-                val['default_code'] = self.env['ir.sequence'].next_by_code('phoenix.optic.alarm')
+                val['default_code'] = self.env['ir.sequence'].next_by_code('phoenix.alarm.pool')
         code = super().create(vals_list)
         return code
